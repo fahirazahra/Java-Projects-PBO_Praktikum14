2adce32b379ce8b80add7e8082a2207df5aa7d8f
Fahira Zahra <71621444+fahirazahra@users.noreply.github.com>
2021-12-08 12:44:44 +0700

    Add files via upload

---
+++ ProjectBahanBakar.java
@@ -0,0 +1,20 @@
+package konsumsibbm;
+
+
+
+public class ProjectBahanBakar {
+
+   public static void main(String[] args) {
+      
+      MobilSedan mAB = new MobilSedan();
+      mAB.tampilHasil();
+        
+      MobilSUV mCD = new MobilSUV();
+      mCD.tampilHasil();
+
+      MobilMPV mEF = new MobilMPV();
+      mEF.tampilHasil();
+      
+   }
+  
+}
